392460ed0120626ebea6b8383fd925fcfe7e707f
HJK <HJKdev@gmail.com>
2020-05-19 22:54:52 +0800

    update setup

--- Makefile
+++ Makefile
@@ -1,14 +1,21 @@
+dev:
+	pip install setuptools wheel pytest black twine flake8
+
 clean:
-	pip uninstall dumpall -y
 	rm -fr build dist .egg *.egg-info
+	rm -fr .pytest_cache coverage.xml report.xml htmlcov
 	find . | grep __pycache__ | xargs rm -fr
 	find . | grep .pyc | xargs rm -f
+
+uninstall:
+	pip uninstall -y dumpall
 
 install:
 	python3 setup.py install
 
+build:
+	python3 setup.py sdist bdist_wheel
+
 publish:
-	pip3 install twine wheel
 	python3 setup.py sdist bdist_wheel
 	twine upload dist/*
-	rm -fr build .egg requests.egg-info
--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@
 # -*- coding:utf-8 -*-
 
 import os
-import sys
 import setuptools
+from os import path
 
-# 'setup.py publish' shortcut.
-if sys.argv[-1] == "publish":
-    os.system("rm -rf dist")
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    sys.exit()
+proj_dir = path.dirname(path.realpath(__file__))
+about_file = path.join(proj_dir, "dumpall", "__version__.py")
 
-here = os.path.abspath(os.path.dirname(__file__))
 about = {}
-with open(os.path.join(here, "dumpall", "__version__.py"), "r", encoding="utf-8") as f:
-    exec(f.read(), about)
+exec(open(about_file, "r", encoding="utf-8").read(), about)
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+long_description = open("README.md", "r", encoding="utf-8").read()
+
+requirements = open("requirements.txt", "r", encoding="utf-8").read().splitlines()
 
 setuptools.setup(
     name=about["__title__"],
@@ -34,7 +29,7 @@ setuptools.setup(
     # include_package_data=True,
     test_suite="tests",
     entry_points={"console_scripts": ["dumpall = dumpall:main"]},
-    install_requires=["click", "aiohttp", "aiomultiprocess"],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
